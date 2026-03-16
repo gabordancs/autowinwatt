b9ca08b20d4ac31dfcca95b1415fe7f800f97964
gabordancs <53614011+gabordancs@users.noreply.github.com>
2026-03-16 21:10:32 +0100

    Add dialog explorer module and integrate runtime dialog mapping

---
+++ winwatt_automation/src/winwatt_automation/dialog_explorer/__init__.py
@@ -0,0 +1,9 @@
+from .dialog_explorer import classify_control, compute_dialog_state_hash, enumerate_dialog_controls, explore_dialog, try_control_interaction
+
+__all__ = [
+    "enumerate_dialog_controls",
+    "classify_control",
+    "try_control_interaction",
+    "compute_dialog_state_hash",
+    "explore_dialog",
+]
